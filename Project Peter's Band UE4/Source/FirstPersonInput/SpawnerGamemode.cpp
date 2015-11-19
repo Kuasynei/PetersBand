22ee8e729da1b3fc01421492e9eebffb27e65d2a
Michael-Nicoletti <mike.e.nicoletti@gmail.com>
2015-11-19 13:22:36 -0500

    Revert "Added Folder Structure"
    This reverts commit 448ef0b268c7f18a85692a878626e4f7af0f23df.

--- Project Peter's Band UE4/Source/FirstPersonInput/SpawnerGamemode.cpp
+++ Project Peter's Band UE4/Source/FirstPersonInput/SpawnerGamemode.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "FirstPersonInput.h"
-#include "Enemies/BaseAI.h"
+#include "BaseAI.h"
 #include "SpawnerGamemode.h"
 
 ASpawnerGamemode::ASpawnerGamemode(const class FObjectInitializer& PCIP)
---
+++ Project Peter's Band UE4/Source/FirstPersonInput/Turtle.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FirstPersonInput.h"
+#include "Turtle.h"
+
+
+// Sets default values
+ATurtle::ATurtle()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Collider = CreateDefaultSubobject<UBoxComponent>(TEXT("Collider"));
+	RootComponent = Collider;
+
+	//Uncomment when the skeletal is given to us instead of the Static Mesh
+	//Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
+	//Mesh->AttachTo(RootComponent);
+
+	StatMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StatMesh"));
+	StatMesh->AttachTo(RootComponent);
+
+}
+
+// Called when the game starts or when spawned
+void ATurtle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATurtle::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+void ATurtle::EnemyInteract(AActor* Interactor)
+{
+	//DO INTERACTIONS HERE
+}
+
+
