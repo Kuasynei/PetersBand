434853babaa26428c7c65e5120ddfb15354df89b
Matthew Ferreiro <matthew.ferreiro@hotmail.com>
2016-01-14 19:12:26 -0500

    Matt_CMT_MainMenuV1.0

--- Project Peter's Band UE4/Source/FirstPersonInput/FirstPersonInput.Build.cs
+++ Project Peter's Band UE4/Source/FirstPersonInput/FirstPersonInput.Build.cs
@@ -6,12 +6,12 @@ public class FirstPersonInput : ModuleRules
 {
 	public FirstPersonInput(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		//PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
-		 PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore", "UMG" });
+		 PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore"  });
 		
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
